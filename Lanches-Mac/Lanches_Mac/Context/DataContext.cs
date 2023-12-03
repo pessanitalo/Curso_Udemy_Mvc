50dad9654278ea75e70ea74f5da2cd8dba3d2dd1
italo pessan <italopessan@gmail.com>
2023-12-03 15:09:01 -0300

    Projeto inicial

---
+++ Lanches-Mac/Lanches_Mac/Context/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Lanches_Mac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches_Mac.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Categoria> Categorias  { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
---
+++ Lanches-Mac/Lanches_Mac/Program.cs
@@ -0,0 +1,47 @@
+using Lanches_Mac.Context;
+using Lanches_Mac.Interface;
+using Lanches_Mac.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches_Mac
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            builder.Services.AddScoped<ILancheRepository, LancheRepository>();
+
+            var app = builder.Build();
+  
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+      
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}

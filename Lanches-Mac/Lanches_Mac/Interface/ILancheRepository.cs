149b68a9ff5fd4ee269fd2518c7e1d85b64f5dd6
italo pessan <italopessan@gmail.com>
2023-12-04 16:41:05 -0300

    adicionado repositorios

---
+++ Lanches-Mac/Lanches_Mac/Interface/ILancheRepository.cs
@@ -0,0 +1,12 @@
+﻿using Lanches_Mac.Models;
+
+namespace Lanches_Mac.Interface
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> ObterLanches();
+        IEnumerable<Lanche> LanchesPreferidos();
+        Lanche ObterLanchePorId(int id);
+
+    }
+}

8277c2890dd3e22865a667f0cb73fc1b60be8a56
italo pessan <italopessan@gmail.com>
2023-12-05 10:58:07 -0300

    migrations, controllers,

---
+++ Lanches-Mac/Lanches_Mac/Controllers/LancheController.cs
@@ -0,0 +1,30 @@
+﻿using Lanches_Mac.Interface;
+using Lanches_Mac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches_Mac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _repository;
+
+        public LancheController(ILancheRepository context)
+        {
+            _repository = context;
+        }
+
+        public IActionResult List()
+        {
+            //var lanches = _repository.ObterLanches();
+            var lanchesListaViewModel = new LancheListaViewModels();
+            lanchesListaViewModel.Lanches = _repository.ObterLanches();
+            lanchesListaViewModel.CategoriaAtual = "Fit";
+            return View(lanchesListaViewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}

94ad67ef35576e6a55f82a7185b75b777063f243
italo pessan <italopessan@gmail.com>
2023-12-07 09:10:06 -0300

    nova migration Pedido

--- Lanches-Mac/Lanches_Mac/Migrations/20231206181211_pedidoDetalhes.Designer.cs
+++ Lanches-Mac/Lanches_Mac/Migrations/20231206181211_pedidoDetalhes.Designer.cs
@@ -4,6 +4,7 @@ using Lanches_Mac.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lanches_Mac.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231206181211_pedidoDetalhes")]
+    partial class pedidoDetalhes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

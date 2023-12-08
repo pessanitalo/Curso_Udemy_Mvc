9de3ab98a6be74da3381ef507b13a84edd469f85
italo pessan <italopessan@gmail.com>
2023-12-08 14:36:54 -0300

    login

---
+++ Lanches-Mac/Lanches_Mac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lanches_Mac.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Campo Obrigatorio")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Password { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}

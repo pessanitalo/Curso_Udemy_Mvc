ec0f258452d22272fcd1b09b7def195b26284250
italo pessan <italopessan@gmail.com>
2023-12-08 14:37:34 -0300

    controller login

---
+++ Lanches-Mac/Lanches_Mac/Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using Lanches_Mac.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches_Mac.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl,
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid) return View(login);
+
+            var user = await _userManager.FindByNameAsync(login.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(login.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    return Redirect(login.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Falha ao realizar o login!");
+            return View(login);
+
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = login.UserName };
+                var result = await _userManager.CreateAsync(user, login.Password);
+
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(login.ReturnUrl))
+                    {
+                        return RedirectToAction("List", "Lanche");
+                    }
+
+                    else
+                    {
+                        ModelState.AddModelError("Registro", "Falha ao cadastrar o login!");
+                    }
+                }
+            }
+
+            return View(login);
+
+        }
+    }
+}

572a81200495c92b0853da3af0a0c0af2e55a892
italo pessan <italopessan@gmail.com>
2023-12-20 10:53:35 -0300

    Detalhes do Pedido

---
+++ Lanches-Mac/Lanches_Mac/ViewModels/PedidoLancheViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Lanches_Mac.Models;
+
+namespace Lanches_Mac.ViewModels
+{
+    public class PedidoLancheViewModel
+    {
+        public Pedido Pedido { get; set; }
+        public IEnumerable<PedidoDetalhe> PedidosDetalhes { get; set; }
+    }
+}
